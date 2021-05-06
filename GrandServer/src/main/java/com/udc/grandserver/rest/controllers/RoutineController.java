f7915a70702f7b875e2ecc1210b1ab75bfec2d2a
Pablo Trillo Valeiro <pablo.trillo@udc.es>
2021-05-06 18:41:07 +0200

    Cambios en el backend

--- GrandServer/src/main/java/com/udc/grandserver/rest/controllers/RoutineController.java
+++ GrandServer/src/main/java/com/udc/grandserver/rest/controllers/RoutineController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
@@ -21,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.udc.grandserver.model.entities.Routine;
 import com.udc.grandserver.model.exceptions.PermissionException;
 import com.udc.grandserver.model.services.RoutineService;
+import com.udc.grandserver.rest.dtos.IdDto;
 import com.udc.grandserver.rest.dtos.RoutineDto;
 
 @RestController
@@ -30,17 +30,12 @@ public class RoutineController {
 	@Autowired
 	private RoutineService routineService;
 	
-	@GetMapping("/getRoutinesByUserId/{id}")
+	@GetMapping("/getRoutinesByUserId")
 	public List<RoutineDto> getRoutinesByUserId(
-			@RequestAttribute Long userId,
-			@PathVariable Long id) throws PermissionException {
-		
-		if (!id.equals(userId)) {
-			throw new PermissionException();
-		}
-		
+			@RequestAttribute Long userId) throws PermissionException {
+				
 		List<RoutineDto> retval = new ArrayList<RoutineDto>();
-		List<Routine> list = this.routineService.getRoutinesByUserId(id);
+		List<Routine> list = this.routineService.getRoutinesByUserId(userId);
 		
 		for (Routine rout : list) {
 			retval.add(toRoutineDto(rout));
@@ -50,7 +45,9 @@ public class RoutineController {
 	
 	@PostMapping("/create")
 	public RoutineDto createRoutine(
+			@RequestAttribute Long userId,
 			@Validated({RoutineDto.AllValidations.class}) @RequestBody RoutineDto routineDto) {
+		routineDto.setUserId(userId);
 		return toRoutineDto(this.routineService.createRoutine(toRoutine(routineDto)));		
 	}
 	
@@ -68,13 +65,9 @@ public class RoutineController {
 	
 	@DeleteMapping()
 	public void deleteRoutine(
-			@RequestAttribute Long userId,
-			@Validated({RoutineDto.UpdateValidations.class}) @RequestBody RoutineDto routineDto) throws PermissionException {
-		if (!routineDto.getUserId().equals(userId)) {
-			throw new PermissionException();
-		}
-		
-		this.routineService.deleteRoutine(routineDto.getId());
+			@RequestBody IdDto idDto) throws PermissionException {
+				
+		this.routineService.deleteRoutine(idDto.getId());
 	}
 	
 }
