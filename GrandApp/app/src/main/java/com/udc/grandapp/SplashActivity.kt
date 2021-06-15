9692fa8d08d13a53973b17f9c8afedc06d751123
Laura Insua Regueiro <laura.insua@udc.es>
2021-06-16 01:20:05 +0200

    Registro+login ya funciona

--- GrandApp/app/src/main/java/com/udc/grandapp/SplashActivity.kt
+++ GrandApp/app/src/main/java/com/udc/grandapp/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.udc.grandapp
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
 import com.udc.grandapp.manager.LoginManager
 import com.udc.grandapp.manager.configuration.UserConfigManager
@@ -15,40 +16,59 @@ import com.udc.grandapp.model.UserInfoModel
 
 class SplashActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash_activity)
-        comprobarUsuarioLogueado()
-    }
+        private val SPLASH_DISPLAY_LENGTH: Long = 3000
 
-    fun comprobarUsuarioLogueado() {
-        val user: UserInfoModel? = UserConfigManager(this).getUserInfoFromBD()
-        if (user == null || (user.userId == "" && user.token == ""))
-            startActivity(Intent(this, MainActivity::class.java))
-        else {
-            actualizarToken(user)
+        override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.splash_activity)
+                UserConfigManager(this).getUserInfoFromBD()
+                //comprobarUsuarioLogueado()
+                startTime()
+
+
         }
-    }
 
-    fun actualizarToken(user: UserInfoModel) {
-        val activity: Activity = this
-        val loginManager = LoginManager(activity)
+        fun startTime(){
+                object: CountDownTimer(SPLASH_DISPLAY_LENGTH,1000){
+                        override fun onTick(millisUntilFinished: Long) {
+                               // TODO("Not yet implemented")
+                        }
 
-        class ResponseManager() : IResponseManagerGeneric {
-            override fun onSuccesResponse(model: GenericModel) {
-                val modelResponse: GenericModel = model as GenericModel
-                if (modelResponse.error == "0") {
-                    val login: SignUpLoginModel = SignUpLoginModel.Parse(modelResponse.json)
-                    UserConfigManager(activity).actualizarTokenBD(user, login.token, activity)
-                    startActivity(Intent(activity, MainScreenActivity::class.java))
-                } else
-                    startActivity(Intent(activity, MainActivity::class.java))
+                        override fun onFinish() {
+                                comprobarUsuarioLogueado()
+                        }
+                }.start()
+        }
+
+        fun comprobarUsuarioLogueado(){
+                val user: UserInfoModel? = UserConfigManager(this).getUserInfoFromBD()
+                if (user == null || (user.userId == "" && user.token == ""))
+                        startActivity(Intent(this, MainActivity::class.java))
+                else {
+                        actualizarToken(user)
+                }
+        }
+
+        fun actualizarToken(user: UserInfoModel) {
+            val activity: Activity = this
+            val loginManager = LoginManager(activity)
+
+            class ResponseManager() : IResponseManagerGeneric {
+                override fun onSuccesResponse(model: GenericModel) {
+                    val modelResponse: GenericModel = model
+                    if (modelResponse.error == "0") {
+                        val login: SignUpLoginModel = SignUpLoginModel.Parse(modelResponse.json)
+                        UserConfigManager(activity).actualizarTokenBD(user, login.token, activity)
+                        startActivity(Intent(activity, MainScreenActivity::class.java))
+                    } else
+                        startActivity(Intent(activity, MainActivity::class.java))
+                }
+
+                override fun onErrorResponse(model: String) {}
             }
 
-            override fun onErrorResponse(model: String) {}
+            val responseManager: IResponseManagerGeneric = ResponseManager()
+            loginManager.realizarOperacion(responseManager, DatosLogin(user.email, user.pwd))
         }
 
-        val responseManager: IResponseManagerGeneric = ResponseManager()
-        loginManager.realizarOperacion(responseManager, DatosLogin(user.email, user.pwd))
     }
-}
